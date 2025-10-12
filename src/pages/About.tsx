3373c4d7f9b0e0585c4e125737b1a272fad94fa6
Samir Hashimov <samirrhashimov>
2025-10-12 15:03:06 +0400

    fix build problem

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowRight, Lock, Share2, Zap, Users, Search, Moon } from 'lucide-react';
 import coverImage from '../assets/blinklogo2.png';
