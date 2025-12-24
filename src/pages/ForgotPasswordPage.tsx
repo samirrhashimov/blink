7675ebbabb4e954af5158a6a067fff67bdb11dbe
Samir Hashimov <samirrhashimov>
2025-12-24 20:13:28 +0400

    upd part 2

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { Mail, CheckCircle, ArrowLeft } from 'lucide-react';
 import blinkLogo from '../assets/blinklogo2.png';
