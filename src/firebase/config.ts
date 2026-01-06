468fc690c4978fc7149133f77979ff6626ec27ba
Samir Hashimov <samirrhashimov>
2026-01-06 23:18:35 +0400

    feat: add google analytics

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,18 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  // These will be replaced with actual Firebase project config
   apiKey: "AIzaSyDLbiffMbrAG94ZhKuenT6zHizIJNTiiWg",
   authDomain: "blink-linknet.firebaseapp.com",
   projectId: "blink-linknet",
   storageBucket: "blink-linknet.firebasestorage.app",
   messagingSenderId: "92174087819",
-  appId: "1:92174087819:web:fdf257e7a784f8fd86068e"
+  appId: "1:92174087819:web:fdf257e7a784f8fd86068e",
+  measurementId: "G-7EHHGHRVY3"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
 export default app;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 
 import App from './App.tsx'
+import { analytics } from './firebase/config'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      {analytics && null}
     </HelmetProvider>
   </StrictMode>,
 )
